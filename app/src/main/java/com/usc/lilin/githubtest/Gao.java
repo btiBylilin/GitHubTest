620a1a09e3969691dfda6765bf34f89287197c0a
lilin <gaosi0812@gmail.com>
2015-08-24 19:20:21 +0800

    add a class by lilin Gao a class a persion

---
+++ app/src/main/java/com/usc/lilin/githubtest/Gao.java
@@ -0,0 +1,14 @@
+package com.usc.lilin.githubtest;
+
+/**
+ * Created by lilin on 2015/8/24 19:18.
+ * Blog: http://gaosililn.iteye.com
+ * e-mail: gaosi0812@gmail.com
+ */
+public class Gao {
+    String name;
+
+    public Gao(String name) {
+        this.name = name;
+    }
+}
